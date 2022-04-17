e0a032fff96998a52d771a915977403096c378b7
Rafael Fontes <rafael@liratech.com.br>
2022-04-16 22:31:15 -0300

    Brincando com variáveis

---
+++ finalizandoAula.js
@@ -0,0 +1,21 @@
+let idade = 25;
+let nome = "Rafael";
+let sobrenome = "Fontes";
+let bacharel = false;
+let dezAnosDepois = 10;
+
+
+let meuNome = "Meu nome: " + nome + " " + sobrenome;
+let minhaIdade = "Minha idade: " + idade;
+if(bacharel == false){
+    var status = " não";
+} else {
+    var status = " sim";
+}
+let formacao = "Bacharelado?" + status;
+let idadeDaqui10Anos = idade + dezAnosDepois;
+
+console.log (meuNome);
+console.log (minhaIdade);
+console.log (formacao);
+console.log (idadeDaqui10Anos);
